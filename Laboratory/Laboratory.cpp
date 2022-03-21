054f62953df165daa94e69633f93be593ba80dfa
SahilM1025 <102006926+SahilM1025@users.noreply.github.com>
2022-03-20 23:23:50 -0400

    Update Laboratory.cpp

--- Laboratory/Laboratory.cpp
+++ Laboratory/Laboratory.cpp
@@ -1,1 +1,20 @@
+//Title: Laboratory.cpp
+//Author: Sahil Misbah
+//Date: 3/15/2022
+//Description: This is the main driver for Laboratory
 
+#include "Lab.h"
+
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <ctime>
+using namespace std;
+
+int main () {
+  srand(time(NULL));
+  Lab newLab;
+  newLab.StartLab();
+  return 0;
+}
